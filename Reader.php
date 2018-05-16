9d94bb4aa5fb9a6caec06cdba9b9d314a4b3744e
Michael Ordaz <ordazmartinezmichael@gmail.com>
2018-05-16 00:32:54 -0500

    el sistema lee un archivo de la carpeta de excel directamente y lo muestra en una tabla en el navegador pero ya inserta los datos en la base de datos

--- Reader.php
+++ Reader.php
@@ -1,34 +1,81 @@
 <?php 
 require 'vendor/autoload.php';
-
-/**
- * 
- */
-class Filas{
 	
-
-	function __construct(){
-		
-	
+	$mysqli = new mysqli("localhost", "root", "", "document");
+	if ($mysqli->connect_errno) {
+	    echo "Falló la conexión a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}
+/*
+	if (!($sentencia = $mysqli->prepare("INSERT INTO test(id) VALUES (?)"))) {
+	    echo "Falló la preparación: (" . $mysqli->errno . ") " . $mysqli->error;
 	}
 
+	$id = 1;
+	if (!$sentencia->bind_param("i", $id)) {
+	    echo "Falló la vinculación de parámetros: (" . $sentencia->errno . ") " . $sentencia->error;
+	}
 
-}
+	if (!$sentencia->execute()) {
+	    echo "Falló la ejecución: (" . $sentencia->errno . ") " . $sentencia->error;
+	}
+*/
 
 $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx');
 $reader->setReadDataOnly(TRUE);
-$spreadsheet = $reader->load("helloworld.xlsx");
+$spreadsheet = $reader->load("excel/test.xlsx");
+
+$worksheet = $spreadsheet->getActiveSheet();
+// Get the highest row and column numbers referenced in the worksheet
+$highestRow = $worksheet->getHighestRow(); // e.g. 10
+$highestColumn = $worksheet->getHighestColumn(); // e.g 'F'
+$highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($highestColumn); // e.g. 5
+
+$query = "";
+
+echo '<table>';
+for ($row = 2; $row <= $highestRow; $row++) {
+
+	$query = "INSERT INTO datos VALUES(";
+
+    echo '<tr>';
+    for ($col = 1; $col <= $highestColumnIndex; $col++) {
+        $value = $worksheet->getCellByColumnAndRow($col, $row)->getCalculatedValue(); //recupera el valor de la celda, parsenado formulas
+        //$value = $worksheet->getCellByColumnAndRow($col, $row)->getValue(); //recupera el valor de la celda incluso la formula
+        
+        $query .= "'".$value."',";
+
+        echo '<td>' . $value . '</td>';
+    }
+    echo '</tr>';
+
+    //acabando el loop formateo bien el string
+    $query = substr( $query, 0, strlen($query)-1 );//quito la ultima coma del string
+    $query .= ")";
+
+    $mysqli->query($query);
+
+}
+echo '</table>';
+
+
+/*
+$reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx');
+$reader->setReadDataOnly(TRUE);
+$spreadsheet = $reader->load("excel/test.xlsx");
 
 $worksheet = $spreadsheet->getActiveSheet();
 echo '<table>';
-$query = "";
+
+$query = "";//declaro mi query vacio
+
 foreach ($worksheet->getRowIterator() as $row) {
-    $query .= "<br>select * from ";//mi linea
+    $query = "INSERT INTO document values (";//mi linea
     echo '<tr>';
     $cellIterator = $row->getCellIterator();
     $cellIterator->setIterateOnlyExistingCells(false);
-    foreach ($cellIterator as $cell) {
-        $query .= $cell->getValue() . " , ";
+    foreach ($cellIterator as $k => $cell) {
+
+        $query .= "'" . $cell->getValue() ."',";
         echo '<td>'.
              $cell->getValue() .
              '</td>';
@@ -40,4 +87,6 @@ echo '</table>';
 
 echo "<hr>";
 echo $query;
+*/
+
 ?>
---
+++ test.sql
@@ -0,0 +1,15 @@
+create database document;
+use document;
+
+-- todos los campos que se requieran
+create table datos(
+	campo1 varchar(100),
+	campo2 varchar(100),
+	campo3 varchar(100),
+	campo4 varchar(100),
+	campo5 varchar(100),
+	campo6 varchar(100),
+	campo7 varchar(100),
+	campo8 varchar(100),
+	campo9 varchar(100)
+)ENGINE=innoDB, charset=utf8;
